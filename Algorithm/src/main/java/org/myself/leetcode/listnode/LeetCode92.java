2529596dd5e097fbcbb13cf0a702a71df2e45b08
mapengfei <mapengfei@idcos.com>
2023-05-26 16:23:18 +0800

    增加以递归方式反转链表

--- Algorithm/src/main/java/org/myself/leetcode/listnode/LeetCode92.java
+++ Algorithm/src/main/java/org/myself/leetcode/listnode/LeetCode92.java
@@ -1,16 +1,67 @@
 package org.myself.leetcode.listnode;
 
+import java.util.List;
+
 /**
  * 反转区间内链表
+ * @author mapengfei
  */
 public class LeetCode92 {
 
-    public static void main(String[] args) {
-        ListNode head = new ListNode(3, new ListNode(5));
-        reverseBetween(head, 1, 2);
+    private static ListNode successor = null;
 
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        //reverseBetween(head, 1, 2);
+        ListNode listNode = reverseBetweenRecursive(head, 2, 4);
+
+        System.out.println(listNode);
     }
 
+    /**
+     * 使用递归方式实现
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public static ListNode reverseBetweenRecursive(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+
+        head.setNext(reverseBetweenRecursive(head.getNext(), left - 1, right - 1));
+
+        return head;
+    }
+
+    /**
+     * 反转前N个链表节点
+     * @param head
+     * @param n
+     * @return
+     */
+    public static ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.getNext();
+            return head;
+        }
+
+        ListNode last = reverseN(head.getNext(), n-1);
+        head.getNext().setNext(head);
+        head.setNext(successor);
+
+        return last;
+    }
+
+
+    /**
+     * 自己的写法
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
     public static ListNode reverseBetween(ListNode head, int left, int right) {
 
         if (head == null || head.getNext() == null) {
